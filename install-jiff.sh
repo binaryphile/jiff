de6b5c4191a959ea0b5a9cd990884b8818673950
Ted Lilley <ted.lilley@gmail.com>
2016-02-09 08:45:26 -0500

    don't clone local

--- install-jiff.sh
+++ install-jiff.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 ! [[ -d "${HOME}/.basher" ]] || exit 0
 pushd "${HOME}" >/dev/null
-echo "I will now fork the jiff repo to your github account and clone it to $HOME/jiff.  Github will prompt you for your password."
+echo "I will now fork the jiff repo to your github account.  Github will prompt you for your password."
 echo
 read -e -p "Username: " username
 read -e -p "OTP (if using 2FA): " otp
@@ -16,7 +16,6 @@ else
   curl -X POST -u "${username}" https://api.github.com/repos/binaryphile/jiff/forks
 fi
 sleep 5
-git clone "git@github.com:${username}/jiff"
 git clone --depth=1 git://github.com/basherpm/basher .basher
 grep -q basher .bashrc || cat <<EOM >> .bashrc
 
@@ -26,8 +25,9 @@ EOM
 cd .basher/libexec
 sed -i -e 's|${bin_path}:${PATH}|${PATH}:${bin_path}|' basher
 sed -i -e 's|$BASHER_ROOT/cellar/bin:$PATH|$PATH:$BASHER_ROOT/cellar/bin|' -e 's|$BASHER_ROOT/cellar/bin $PATH|$PATH $BASHER_ROOT/cellar/bin|' basher-init
-popd >/dev/null
 "${HOME}/.basher/bin/basher" install "${username}/jiff"
+cd "${HOME}/.basher/cellar/packages/${username}/jiff"
+git remote set-url origin "git@github.com:${username}/jiff"
 cat <<EOM
 
 Note: If you are using bash as your shell, run "exec bash" to load jiff
@@ -38,3 +38,4 @@ re-login.
 You'll want to run "jiff use auto" to set up platform-specific tasks.
 
 EOM
+popd >/dev/null
